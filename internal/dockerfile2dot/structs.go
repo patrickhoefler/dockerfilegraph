e8a8e3e508ea7f04968552765e73ab5f76b12a2b
Patrick Hoefler <patrick.hoefler@gmail.com>
2021-02-21 13:00:35 +0100

    Add comments to Stage struct

--- internal/dockerfile2dot/structs.go
+++ internal/dockerfile2dot/structs.go
@@ -9,8 +9,9 @@ type SimplifiedDockerfile struct {
 // Stage contains the parts of a single build stage within a multi-stage Dockerfile
 // that are relevant for generating the multi-stage build graph
 type Stage struct {
-	ID      string
-	Name    string
-	Args    []string
-	WaitFor []string `yaml:"waitFor"`
+	ID       string   // numeric identifier for internal stages, full repo:tag@sha for external base images
+	Name     string   // alias (the part after the AS) for internal stages
+	Args     []string // not currently used, might be removed
+	WaitFor  []string `yaml:"waitFor"` // IDs of stages or external images that the stage depends on
+	External bool     // true if the stage is actually an external base image
 }
