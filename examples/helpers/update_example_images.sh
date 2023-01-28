3384ebccd8b0522d3fe00289be7c25a37be2be8a
Patrick Hoefler <patrick.hoefler@gmail.com>
2023-01-28 18:46:23 +0100

    Fix max label length

---
+++ examples/helpers/update_example_images.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Change to the root directory of the project.
+cd $(git rev-parse --show-toplevel)
+
+go run . -f examples/dockerfiles/Dockerfile --legend -o svg \
+  && mv Dockerfile.svg examples/images/Dockerfile-legend.svg
+
+go run . -f examples/dockerfiles/Dockerfile --layers -o svg \
+  && mv Dockerfile.svg examples/images/Dockerfile-layers.svg
+
+go run . -f examples/dockerfiles/Dockerfile.large -c -u 4 -o svg \
+  && mv Dockerfile.svg examples/images/Dockerfile-large.svg
